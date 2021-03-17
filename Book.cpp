bd878f3be0b0fbc067d052847996c595a1dd88b9
David <DA924@live.mdx.ac.uk>
2021-03-18 03:46:13 +0400

    Instantiating functions in book class; Adding Book.h; have not populated the functions yet

---
+++ Book.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "Book.h"
+template<class T>
+Book<T>::Book(){
+
+}
+
+template<class T>
+T Book<T>::addBook(){
+
+}
+
+template<class T>
+void Book<T>::searchBook(){
+
+}
+
+template<class T>
+T Book<T>::removeBook(){
+
+}
+int main () {
+  
+  return 0;
+}
