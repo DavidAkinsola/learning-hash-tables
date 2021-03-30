964a313a6f1aec17dd51f9fc0ee042ff946fcfdc
David <DA924@live.mdx.ac.uk>
2021-03-30 05:05:38 +0400

    Adding hash class...

--- Book.cpp
+++ Book.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <list>
 
+#include "hash.cpp"
 #include "Book.h"
+
+
 template<class T>
-Book<T>::Book(){
+Book<T>::Book(T title, T author, int ISBN, int quantity ){
+  this->title = title;
+  this->author = author;
+  this->ISBN = ISBN;
+  this->quantity = quantity;
 
 }
 
@@ -22,6 +30,7 @@ template<class T>
 T Book<T>::removeBook(){
 
 }
+
 int main () {
   
   return 0;
