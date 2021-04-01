e25660480a087ce5116bf7005f06483975fb68db
David <DA924@live.mdx.ac.uk>
2021-04-01 05:39:35 +0400

    search function implemented...collision issues detected...

--- hash.cpp
+++ hash.cpp
@@ -13,7 +13,7 @@ class Hash{
       int function(int key);
       void insertItem(int key, std::string t);
       void removeItem(int key);
-      std::string searchItem(int key);
+      void searchItem(int key);
       void getTable();
       
 
@@ -77,4 +77,77 @@ void Hash::removeItem(int key){
     }
 
     return;
+}
+
+void Hash:: searchItem(int key){
+    int hashVal = function(key);
+    auto& cell = table[hashVal];
+    auto bItr = begin(cell);//iteration from the begining
+    bool keyExists = false;
+    for(; bItr != end(cell); bItr++){
+        if(bItr->first == key){
+            keyExists = true;
+            std::cout<< "\n\nsearch results...";
+            std::cout<< "key: " <<bItr->first<<" value: "<<bItr->second<<std::endl;
+            break;
+
+        }
+    }
+
+    if(!keyExists){
+        std::cout<<"key not found" <<std::endl;
+    }
+
+    return;
+}
+
+void Hash::getTable(){
+    for(int i{}; i<hashgroup; i++){
+        if(table[i].size()==0) continue;
+
+        auto bItr = table[i].begin();
+        for(; bItr!=table[i].end(); bItr++){
+            std::cout<< "key: " <<bItr->first<<"value: "<<bItr->second<<std::endl;
+        }
+    }
+    return;
+}
+
+int main(){
+    Hash h;
+
+    if(h.isEmpty()){
+        std::cout<<"success";
+    }else{
+        std::cout<<"failed"<<std::endl;
+    }
+
+    h.insertItem(155, "a");
+    h.insertItem(156, "b");
+    h.insertItem(153, "c");
+    h.insertItem(167, "d");
+    h.insertItem(138, "e");
+    h.insertItem(148, "f");
+    h.insertItem(169, "g");
+    h.insertItem(177, "h");
+    h.insertItem(120, "i");
+    h.insertItem(131, "j");
+    h.insertItem(191, "k");
+
+    h.getTable();
+
+    h.searchItem(169);
+
+    
+
+    if(h.isEmpty()){
+        std::cout<<"failed"<<std::endl;
+    }else{
+        std::cout<<"success"<<std::endl;
+    }
+
+
+    return 0;
+
+
 }
